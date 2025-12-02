30625ec49ea9b2d0dce2e4072c06ff632d6e8fbe
Zaire Ali <zaireali649@gmail.com>
2025-12-01 19:42:51 -0500

    Update list_vpc_ids.py

--- list_vpc_ids.py
+++ list_vpc_ids.py
@@ -1,10 +1,14 @@
-import boto3
+import boto3  # AWS SDK for Python, used to interact with AWS services
 
+# Create an EC2 client to interact with VPC-related APIs
 vpc_client = boto3.client('ec2')
 
+# Retrieve metadata for all VPCs in the current AWS account/region
 response = vpc_client.describe_vpcs()
 
+# Extract the list of VPC objects from the API response
 vpcs = response['Vpcs']
 
+# Loop through each VPC and print its unique identifier
 for vpc in vpcs:
     print(vpc['VpcId'])
